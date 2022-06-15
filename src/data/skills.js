1011f8d5fcd9a90d68f783f7356384381e4e80af
Ramkrishna732 <ramkrishnamaurya732@gmail.com>
2022-06-15 10:53:40 +0530

    favicon and skills

--- src/data/skills.js
+++ src/data/skills.js
@@ -2,21 +2,23 @@ export const SkillsData = [
   {
     type: "Frontend",
     list: [
-      {
-        name: "ReactJS",
-        icon: <i class="devicon-react-original colored"></i>,
-      },
+      
       {
         name: "Html",
         icon: <i class="devicon-html5-plain colored"></i>,
+      },
+
+      {
+        name: "Css",
+        icon: <i class="devicon-css3-plain colored"></i>,
       },
       {
         name: "Javascript",
         icon: <i class="devicon-javascript-plain colored"></i>,
       },
       {
-        name: "Css",
-        icon: <i class="devicon-css3-plain colored"></i>,
+        name: "ReactJS",
+        icon: <i class="devicon-react-original colored"></i>,
       },
     ],
   },
@@ -30,6 +32,10 @@ export const SkillsData = [
       {
         name: "NodeJS",
         icon: <i class="devicon-nodejs-plain colored"></i>,
+      },
+      {
+        name: "Express",
+        icon: <i class="devicon-express-original colored"></i>,
       },
     ],
   },
