979a5b48f7f720ce99048d296778a46be4e76f0d
Ramkrishna732 <ramkrishnamaurya732@gmail.com>
2022-06-16 16:32:53 +0530

    prject added

--- src/components/body/projects/project-card.js
+++ src/components/body/projects/project-card.js
@@ -29,7 +29,10 @@ function ProjectCard({ project }) {
           })}
         </div>
       </div>
-      <img src={project.image} className="project-photo" />
+     
+      <img src={project.image} className="project-photo" alt="projectimage" />
+      
+
     </div>
   );
 }
